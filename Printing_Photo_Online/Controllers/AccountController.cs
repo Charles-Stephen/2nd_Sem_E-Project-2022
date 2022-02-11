2968554d8025d83e5ae46f41347a15ce4042b76e
Charles-Stephen <charlesadwin99@gmail.com>
2022-02-11 14:26:38 -0800

    kl
    l

--- Printing_Photo_Online/Controllers/AccountController.cs
+++ Printing_Photo_Online/Controllers/AccountController.cs
@@ -75,6 +75,139 @@ namespace Printing_Photo_Online.Controllers
 
 
         //==============================================================
+        //              EDIT
+        //==============================================================
+
+        public ActionResult Edit(int? id)
+        {
+            if (Session["name"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                user data = db.users.Find(id);
+
+                if (data == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+                }
+                else
+                {
+                    return View(data);
+                }
+                return View(data);
+            }
+            else if (Session["name"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection fc, HttpPostedFileBase image)
+        {
+            user data = db.users.Find(id);
+
+
+            var filename = Path.GetFileNameWithoutExtension(image.FileName);
+            var exten = Path.GetExtension(image.FileName);
+            Random rnd = new Random();
+
+            var myimg = filename + rnd.Next() + exten;
+
+            image.SaveAs(@"D:\E-Project\Printing_Photo_Online\profile_img\" + myimg);
+
+            data.id = int.Parse(Request.Form["id"]);
+            data.First_Name = Request.Form["firstname"];
+            data.Last_Name = Request.Form["lastname"];
+            data.Email = Request.Form["email"];
+            data.Pass = Request.Form["pass"];
+            data.Date_Of_Birth = Request.Form["birth"];
+            data.Gender = Request.Form["gender"];
+            data.Phone = Request.Form["phone"];
+            data.User_Profile = myimg;
+
+            db.Entry(data).State = EntityState.Modified;
+            db.SaveChanges();
+
+            var x = Session["usertype"];
+            switch (x)
+            {
+                case 0:
+                    return RedirectToAction("Index", "Account");
+                    break;
+                case 1:
+                    return RedirectToAction("Index", "Home");
+                    break;
+            }
+            return View();
+        }
+
+
+
+
+        //==============================================================
+        //              DELETE
+        //==============================================================
+
+        public ActionResult Delete(int? id)
+        {
+            ViewBag.yourtype = Session["usertype"];
+
+            if (Session["name"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                user data = db.users.Find(id);
+                if (data == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+                }
+                else
+                {
+                    return View(data);
+                }
+            }
+            else if (Session["name"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+        [HttpPost, ActionName("Delete")]
+
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            user data = db.users.Find(id);
+
+            db.users.Remove(data);
+            db.SaveChanges();
+
+
+            var x = Session["usertype"];
+            switch (x)
+            {
+                case 0:
+                    return RedirectToAction("Index", "Account");
+                    break;
+                case 1:
+                    return RedirectToAction("Index", "Home");
+                    break;
+            }
+            return View();
+
+        }
+
+
+
+
+        //==============================================================
         //              LOGIN
         //==============================================================
 
